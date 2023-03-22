8bc57af5b1dc87e15dc812355045b3867ec82096
Gutek8134 <gutek8134@gmail.com>
2023-03-22 20:29:04 +0100

    Spawning cards for adding to deck

--- Source/Karcianka/KarciankaGameModeBase.cpp
+++ Source/Karcianka/KarciankaGameModeBase.cpp
@@ -6,3 +6,80 @@
 void AKarciankaGameModeBase::InitGame(const FString& MapName, const FString& Options, FString& ErrorMessage) {
 	Super::InitGame(MapName, Options, ErrorMessage);
 }
+
+TArray<TSubclassOf<ACard>> AKarciankaGameModeBase::FGenerateRandomObtainableCards(uint8 number) {
+	// Number must be lesser or equal than the number of card types
+	if (ObtainableCards.Num() < number) {
+		UE_LOG(LogTemp, Error, TEXT("Game mode/Generate random obtainable cards: too much cards to generate (%i/%i)"), number, ObtainableCards.Num());
+		throw std::invalid_argument("Number is too high");
+	}
+	// If it is equal, all types are returned
+	else if (ObtainableCards.Num() == number) {
+		TArray<TSubclassOf<ACard>> temp;
+		ObtainableCards.GenerateKeyArray(temp);
+		return temp;
+	}
+
+
+	TArray<TSubclassOf<ACard>> temp;
+	
+	auto cardsWeight = GetObtainableCardsWeight();
+	auto ObtainableCopy = ObtainableCards;
+	
+	
+	for (auto i = 0; i < number; ++i) {
+		auto rnd = FMath::FRandRange(0, cardsWeight);
+		
+		// Ensures every card will appear only once
+		for (const auto [key, value] : ObtainableCopy) {
+			if (rnd <= value) {
+				temp.Add(key);
+				cardsWeight -= ObtainableCopy[key];
+				ObtainableCopy.Remove(key);
+			}
+			else rnd -= value;
+		}
+	}
+
+	return temp;
+}
+
+void AKarciankaGameModeBase::GenerateRandomObtainableCards(TArray<TSubclassOf<ACard>>& temp, uint8 number) {
+	// Number must be lesser or equal than the number of card types
+	if (ObtainableCards.Num() < number) {
+		UE_LOG(LogTemp, Error, TEXT("Game mode/Generate random obtainable cards: too much cards to generate (%i/%i)"), number, ObtainableCards.Num());
+		return;
+	}
+	// If it is equal, all types are returned
+	else if (ObtainableCards.Num() == number) {
+		ObtainableCards.GenerateKeyArray(temp);
+	}
+
+	auto cardsWeight = GetObtainableCardsWeight();
+	auto ObtainableCopy = ObtainableCards;
+
+
+	for (auto i = 0; i < number; ++i) {
+		auto rnd = FMath::FRandRange(0, cardsWeight);
+
+		// Ensures every card will appear only once
+		for (const auto [key, value] : ObtainableCopy) {
+			if (rnd <= value) {
+				temp.Add(key);
+				cardsWeight -= ObtainableCopy[key];
+				ObtainableCopy.Remove(key);
+			}
+			else rnd -= value;
+		}
+	}
+}
+
+uint16 AKarciankaGameModeBase::GetObtainableCardsWeight() {
+	TArray<uint8> values;
+	ObtainableCards.GenerateValueArray(values);
+	uint16 temp = 0;
+	for (const auto& e : values) {
+		temp += e;
+	}
+	return temp;
+}
--- Source/Karcianka/KarciankaGameModeBase.h
+++ Source/Karcianka/KarciankaGameModeBase.h
@@ -2,13 +2,14 @@
 
 #pragma once
 
+#include <stdexcept>
 #include "CoreMinimal.h"
 #include "Card.h"
 #include "GameFramework/GameModeBase.h"
 #include "KarciankaGameModeBase.generated.h"
 
 /**
- * 
+ *
  */
 UCLASS()
 class KARCIANKA_API AKarciankaGameModeBase : public AGameModeBase
@@ -16,4 +17,16 @@ class KARCIANKA_API AKarciankaGameModeBase : public AGameModeBase
 	GENERATED_BODY()
 public:
 	virtual void InitGame(const FString& MapName, const FString& Options, FString& ErrorMessage) override;
+
+protected:
+	//Weighed list card: chance to get
+	UPROPERTY(BlueprintReadWrite, EditDefaultsOnly)
+		TMap<TSubclassOf<ACard>, uint8> ObtainableCards;
+
+	UFUNCTION(BlueprintCallable)
+		TArray<TSubclassOf<ACard>> FGenerateRandomObtainableCards(uint8 number);
+	UFUNCTION(BlueprintCallable)
+		void GenerateRandomObtainableCards(TArray<TSubclassOf<ACard>>& temp, uint8 number);
+	
+	uint16 GetObtainableCardsWeight();
 };
